39ac2de1ec0b1dc9061bab8cde85eed4956f6418
yadhap dahal <yadhapdahal@yadhaps-MBP.attlocal.net>
2022-05-15 12:09:27 -0400

    edited

--- app.js
+++ app.js
@@ -1,9 +1,9 @@
-const express = require('express');
+const express = require("express");
 const app = express();
 
-app.get('/', (req, res) =>{
-res.send('Hello world');
-})
-app.listen(3000, () =>{
-console.log('App listining at port 3000');
+app.get("/", (req, res) => {
+  res.send("Hello world");
+});
+app.listen(3000, () => {
+  console.log("App listining at port 3000 - Edited");
 });
